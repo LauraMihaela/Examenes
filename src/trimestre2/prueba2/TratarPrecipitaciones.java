f5898576153c670996375d13540cd5d159e01b5d
Laura Gheorghiu <gheorghiulaura@yahoo.com>
2022-03-13 15:44:55 +0100

    Método obtenerPrecipitacionAnual de la clase TratarPrecipitaciones

--- src/trimestre2/prueba2/TratarPrecipitaciones.java
+++ src/trimestre2/prueba2/TratarPrecipitaciones.java
@@ -4,8 +4,18 @@ public class TratarPrecipitaciones {
 	
 	static final int MESES=12;
 
-	public static int[] obtenerPrecipitacionAnual (int[][] precipitaciones) {
-		return null;
+	public static int[] obtenerPrecipitacionAnual(int[][] precipitaciones) {
+		int precipitacionAnual[] = new int[precipitaciones.length];
+
+		int suma = 0;
+		for (int i = 0; i < precipitaciones.length; i++) { // recorre filas-localidades
+			for (int j = 0; j < precipitaciones[0].length; j++) {// recorrido en cada fila
+				suma += precipitaciones[i][j];
+			} // termino de recorrer la fila
+			precipitacionAnual[i] = suma;
+			suma = 0;
+		}
+		return precipitacionAnual;
 	}
 	
 	public static int[] obtenerPromedioMensual (int[][] precipitaciones) {
